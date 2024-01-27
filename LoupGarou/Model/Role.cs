70ddb4146a5ba57a8c2c0cbd481ef6604d67a349
BenAyedMehdi <mehdibenayed97@gmail.com>
2024-01-27 03:19:06 +0100

    add properties to role model

--- LoupGarou/Model/Role.cs
+++ LoupGarou/Model/Role.cs
@@ -1,8 +1,10 @@
 ﻿namespace LoupGarou.Model
 {
-  public class Role
-  {
-    public Guid RoleId { get; set; }
-    public string RoleName { get; set; }
-  }
+    public class Role
+    {
+        public Guid RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string Description { get; set; }
+        public string Ability{ get; set; }
+    }
 }
